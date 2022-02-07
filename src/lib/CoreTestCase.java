8fbc7877f752ad82c0310633a5ba949c700db15e
kurbatova.ea <kurbatova.ea@sibvaleo.com>
2022-02-07 15:10:23 +0700

    VII.01 - Создание метода Platform и подключение skip

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,29 +1,23 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
+
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
+    protected Platform Platform;
 
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
     @Override
     protected void setUp() throws Exception{
-
         super.setUp();
-
-        driver = initDriver();
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
         this.rotateScreenPortrait();
     }
 
@@ -46,41 +40,5 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(Duration.ofSeconds(seconds));
     }
 
-    private AppiumDriver initDriver() throws Exception {
-        String platform = System.getenv("PLATFORM");
-        URL url = new URL(AppiumURL);
-        switch (platform){
-            case PLATFORM_ANDROID:
-                return new AndroidDriver(url, getCapabilitiesByPlatformEnv());
-            case PLATFORM_IOS:
-                return new IOSDriver(url, getCapabilitiesByPlatformEnv());
-            default:
-                throw new RuntimeException("Неизсветный тип платформы, " + platform);
-        }
-    }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception
-    {
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if(platform.equals(PLATFORM_ANDROID)) {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "8.0");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "/home/lena/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone SE");
-            capabilities.setCapability("platformVersion", "11.3");
-            capabilities.setCapability("app", "/home/lena/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value - " + platform);
-        }
-
-        return capabilities;
-    }
 }
